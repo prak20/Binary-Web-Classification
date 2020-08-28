0599c8afb75c3232b29f746a7c2ffcca36e6a6e9
prak20 <56407880+prak20@users.noreply.github.com>
2020-08-28 15:51:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 mkdir -p ~/.streamlit/
-
+echo "\
+[general]\n\
+email = \"prakhar.15it@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
